06cedfba2250dcbe63c27860c80715dbae80988f
Adeyinka Inioluwa John <boluwatifeeri@gmail.com>
2023-03-27 13:47:23 -0700

    feat:created backend

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
+import store from './store';
+import './index.css'
+import './style.css';
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+
+
+createApp(App) .use(store) .mount( '#app')
